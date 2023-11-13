48071719e4bcfe5083e900b098e2ff10a0a59d2a
Mathys <mathys.haubert80@gmail.com>
2023-11-13 08:37:32 +0100

    azerty

--- src/Controller/PresentationPageController.php
+++ src/Controller/PresentationPageController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,8 +17,34 @@ class PresentationPageController extends AbstractController
      * @param string $page
      * @return Response
      */
-    public function presentationPage(string $page): Response
-    {
-        return $this->render('templates/presentationPage/presentation.html.twig',["whatSite" => $page]);
+    public function presentationPage(Request $request): Response
+    {   
+        
+        // $_GET parameters
+        $who = $request->query->get('who');
+        if($who ==="company"){
+            $link = [
+                "img1" => "img/img_presentation_alternant/img_1.png",
+                "img2" => "img/img_presentation_alternant/img_2.png",
+                "img3" => "img/img_presentation_alternant/img_3.png",
+                "alt1" => "Préparation des Candidat(e)s",
+                "alt2" => "Placement en Alternance",
+                "alt3" => "Suivi Continu",
+                "text" => "Nous vous aidons à décrocher l'alternant(e) de vos rêves. Notre équipe dédiée offre un accompagnement personnalisé, des ressources pour optimiser votre recrutement et un suivi continu pour assurer le succès de cette collaboration."
+            ];
+        }
+        else if($who === "intern"){
+            $link = [
+                "img1" => "img/img_presentation_company/img_1.png",
+                "img2" => "img/img_presentation_company/img_2.png",
+                "img3" => "img/img_presentation_company/img_3.png",
+                "alt1" => "Mise en relation avec des candidats qualifiés",
+                "alt2" => "Organisation d'entretiens",
+                "alt3" => "Placement au sein de votre entreprise rapidement",
+                "text" => "Nous vous aidons à décrocher le contrat d'alternance de vos rêves. Notre équipe dédiée offre un accompagnement personnalisé, des ressources pour optimiser votre candidature et un suivi continu pour assurer votre succès"
+            ];
+        }
+        return $this->render('presentationPage\presentation.html.twig',$link);
+    
     }
 }
